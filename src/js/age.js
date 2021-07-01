e78ba56741497a92c35bbd92ffc9376b5bb3b0f3
Kevin Funez <kevin.funez315@gmail.com>
2021-06-30 20:48:16 -0700

    return age in Venus

--- src/js/age.js
+++ src/js/age.js
@@ -1,6 +1,7 @@
 export default class Age {
   constructor(age) {
     this.userAge = age;
-    this.mercuryAge = age * .24; 
+    this.mercuryAge = age * .24;
+    this.venusAge = age * .62;
   }
 }

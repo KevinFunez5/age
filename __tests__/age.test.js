c72be46c34ab25ce03b70374fc19817a68a5f556
Kevin Funez <kevin.funez315@gmail.com>
2021-06-30 20:32:51 -0700

    git commit -m

---
+++ __tests__/age.test.js
@@ -0,0 +1,13 @@
+import Age from './../src/js/age.js'
+describe('Age', () => {
+
+  test('should output the users age', () => {
+    const age = new Age(20);
+    expect(age.userAge).toEqual(20);
+  });
+
+  test('should output Mercury age', () => {
+    const age = new Age(20)
+    expect(age.mercuryAge).toEqual(4.8)
+  });
+});
---
+++ src/js/age.js
@@ -0,0 +1,6 @@
+export default class Age {
+  constructor(age) {
+    this.userAge = age;
+    this.mercuryAge = age * .24; 
+  }
+}
